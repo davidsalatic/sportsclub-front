a8dc81f0a5245a3e7a714dad18c4e94a96a7443e
David Salatic <davidsalatic@live.com>
2020-03-12 16:26:46 +0100

    Menu items hide/show depending on logged in Role

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AppUserService } from './services/app-user-service';
+import { AuthService } from './services/auth-service';
+import { Roles } from './const/role-const';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,9 @@ export class AppComponent implements OnInit {
   title = 'Sports Club';
 
   showTrainingSessions:boolean=false;
+  showMemberships:boolean=false;
   showMembers:boolean=false;
+  
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -21,11 +25,39 @@ export class AppComponent implements OnInit {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver,private appUserService:AppUserService) {
+  constructor(private breakpointObserver: BreakpointObserver,private authService:AuthService) {
   }
 
   ngOnInit() {
-    
+    console.log("app component oinit")
+    this.authService.getToken().subscribe(token=>{
+      this.authService.extractClaims(token).subscribe(claims=>{
+        if(claims.role===Roles.MEMBER)
+        {
+          this.showTrainingSessions=false;
+          this.showMemberships=false;
+          this.showMembers=false;
+        }
+        else if(claims.role===Roles.COACH)
+        {
+          this.showTrainingSessions=true;
+          this.showMemberships=false;
+          this.showMembers=true;
+        }
+        else if(claims.role===Roles.MANAGER)
+        {
+          this.showTrainingSessions=true;
+          this.showMembers=true;
+          this.showMemberships=true;
+        }
+        else
+        {
+          this.showTrainingSessions=false;
+          this.showMembers=false;
+          this.showMemberships=false;
+        }
+      })
+    })
   }
 
 
