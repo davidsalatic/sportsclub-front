0ff0dc4f194c49b334e4ce8d4a2ae60341161bb8
David Salatic <davidsalatic@live.com>
2020-04-15 23:25:45 +0200

    Update http-interceptor.service.ts

--- src/app/interceptor/http-interceptor.service.ts
+++ src/app/interceptor/http-interceptor.service.ts
@@ -13,7 +13,7 @@ export class HttpInterceptorService {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       let headers;
-      if (req.url.includes('auth') || req.url.includes('period')) {
+      if (req.url.includes('auth')) {
           headers = req.headers.set('Content-Type', 'application/json');
       } else {
           headers = req.headers
